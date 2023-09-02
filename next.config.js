6e4cb42d7a20dd953e71955c24ddc2d7ffdfd7bd
Yousif Abozid <yousif.abozid@yahoo.com>
2023-09-02 23:05:36 +0300

    add prettier and eslint configuration files and run format

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	experimental: { appDir: true },
+	/**
+	 * Updates the webpack configuration by enabling top level await.
+	 *
+	 * @param {object} config - The webpack configuration object.
+	 * @return {object} The updated webpack configuration object.
+	 */
+	webpack(config) {
+		config.experiments = {
+			...config.experiments,
+			topLevelAwait: true,
+		}
+		return config
+	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// Your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
+	// DistDir: 'build', // Create the dist directory for the build
+	// Output: 'standalone', // Can be deployed on its own without installing node_modules.
+}
 
 module.exports = nextConfig
