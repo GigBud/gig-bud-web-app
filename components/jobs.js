054e08e667f974af19b7402374f5a9cbee8bfa3d
nouranhesham98 <noranhesham46@gmail.com>
2023-09-09 04:09:38 +0300

    Edited  Jobs

--- components/jobs.js
+++ components/jobs.js
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import Image from 'next/image'
+import { Row } from 'react-bootstrap'
 
 export default function Jobs() {
 	return (
@@ -26,7 +27,7 @@ export default function Jobs() {
 					</details>
 
 					<details open className={styles.details} width={50}>
-						<summary  className={`${styles['details-design']}`}>Design</summary>
+						<summary className={`${styles['details-design']}`}>Design</summary>
 						<ul>
 							<li className={styles.designLi}>
 								<span className={styles.span}>UX Desiner</span>
@@ -49,193 +50,161 @@ export default function Jobs() {
 				{/* Job Cards Section  */}
 
 				<div className={styles.cards}>
-					{/* First Card  */}
-					<div className={styles.card}>
-						{/* Card Icon , Title , Position*/}
-						<div className={styles.header}>
-							<Image
-								className={styles.card_icon}
-								width={60}
-								height={50}
-                                
-                                src="/images/jobs/first.png"
-								alt=""
-							/>
-							<div className={styles.card_info}>
-								<h2 className={styles.card_title}>UX Designer</h2>
-                                <FontAwesomeIcon  className={styles.plus}icon={faPlus} />
-								<span>Senior</span>
+					<div className={styles.row}>
+						{/* First Card  */}
+						<div className={styles.card}>
+							{/* Card Icon , Title , Position*/}
+							<div className={styles.header}>
+								<Image className={styles.card_icon} width={60} height={50} src="/images/jobs/first.png" alt="" />
+								<div className={styles.card_info}>
+									<h2 className={styles.card_title}>UX Designer</h2>
+									<FontAwesomeIcon className={styles.plus} icon={faPlus} />
+									<span>Senior</span>
+								</div>
 							</div>
-						</div>
-						{/* Job Location */}
-						<div className={styles.location}>
-							<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
-							<p className={styles.description}>
-								<span>Remote /</span> office in Ottawa , Ontario
-							</p>
-						</div>
-						{/*Card Body */}
-						<p className={styles.card_body}>
-							Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
-							dummy text ever.
-						</p>
-					</div>
-
-					{/* Second Card  */}
-					<div className={styles.card}>
-						{/* Card Icon , Title , Position*/}
-						<div className={styles.header}>
-							<Image
-								className={styles.card_icon}
-								width={60}
-								height={50}
-								src="/images/jobs/second.png"
-								alt=""
-							/>
-
-							<div className={styles.card_info}>
-								<h2 className={styles.card_title}>UI Designer</h2>
-                                <FontAwesomeIcon  className={styles.plus}icon={faPlus} />
-								<span>Junior</span>
-                               
+							{/* Job Location */}
+							<div className={styles.location}>
+								<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
+								<p className={styles.description}>
+									<span>Remote /</span> office in Ottawa , Ontario
+								</p>
 							</div>
-						</div>
-						{/* Job Location */}
-						<div className={styles.location}>
-							<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
-							<p className={styles.description}>
-								<span>Remote</span> office in Kyiv , Ukraine
+							{/*Card Body */}
+							<p className={styles.card_body}>
+								Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
+								dummy text ever.
 							</p>
 						</div>
-						{/*Card Body */}
-						<p className={styles.card_body}>
-							Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
-							dummy text ever.
-						</p>
-					</div>
 
-					{/* Third Card*/}
-					<div className={styles.card}>
-						{/* Card Icon , Title , Position*/}
-						<div className={styles.header}>
-							<Image
-								className={styles.card_icon}
-								width={60}
-								height={50}
-								src="/images/jobs/third.png"
-								alt=" "
-							/>
-							<div className={styles.card_info}>
-								<h2 className={styles.card_title}>UI Designer</h2>
-                                <FontAwesomeIcon  className={styles.plus}icon={faPlus} />
-								<span>Mid-Senior</span>
+						{/* Second Card  */}
+						<div className={styles.card}>
+							{/* Card Icon , Title , Position*/}
+							<div className={styles.header}>
+								<Image className={styles.card_icon} width={60} height={50} src="/images/jobs/second.png" alt="" />
+
+								<div className={styles.card_info}>
+									<h2 className={styles.card_title}>UI Designer</h2>
+									<FontAwesomeIcon className={styles.plus} icon={faPlus} />
+									<span>Junior</span>
+								</div>
 							</div>
-						</div>
-						{/* Job Location */}
-						<div className={styles.location}>
-							<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
-							<p className={styles.description}>
-								<span>Remote</span> office in Lagos , Nigria
+							{/* Job Location */}
+							<div className={styles.location}>
+								<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
+								<p className={styles.description}>
+									<span>Remote</span> office in Kyiv , Ukraine
+								</p>
+							</div>
+							{/*Card Body */}
+							<p className={styles.card_body}>
+								Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
+								dummy text ever.
 							</p>
 						</div>
-						{/*Card Body */}
-						<p className={styles.card_body}>
-							Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
-							dummy text ever.
-						</p>
 					</div>
 
-					{/* Fourth Card*/}
-					<div className={styles.card}>
-						{/* Card Icon , Title , Position*/}
-						<div className={styles.header}>
-							<Image
-								className={styles.card_icon}
-								width={60}
-								height={50}
-								src="/images/jobs/fourth.png"
-								alt=""
-							/>
-							<div className={styles.card_info}>
-								<h2 className={styles.card_title}>UX Writer</h2>
-                                <FontAwesomeIcon  className={styles.plus}icon={faPlus} />
-								<span>Mid-Level</span>
-							</div>{' '}
-						</div>
-						{/* Job Location */}
-						<div className={styles.location}>
-							<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
-							<p className={styles.description}>
-								<span>Remote</span> office in Lagos , Nigria
+					<div className={styles.row}>
+						{/* Third Card*/}
+						<div className={styles.card}>
+							{/* Card Icon , Title , Position*/}
+							<div className={styles.header}>
+								<Image className={styles.card_icon} width={60} height={50} src="/images/jobs/third.png" alt=" " />
+								<div className={styles.card_info}>
+									<h2 className={styles.card_title}>UI Designer</h2>
+									<FontAwesomeIcon className={styles.plus} icon={faPlus} />
+									<span>Mid-Senior</span>
+								</div>
+							</div>
+							{/* Job Location */}
+							<div className={styles.location}>
+								<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
+								<p className={styles.description}>
+									<span>Remote</span> office in Lagos , Nigria
+								</p>
+							</div>
+							{/*Card Body */}
+							<p className={styles.card_body}>
+								Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
+								dummy text ever.
 							</p>
 						</div>
-						{/*Card Body */}
-						<p className={styles.card_body}>
-							Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
-							dummy text ever.
-						</p>
+
+						{/* Fourth Card*/}
+						<div className={styles.card}>
+							{/* Card Icon , Title , Position*/}
+							<div className={styles.header}>
+								<Image className={styles.card_icon} width={60} height={50} src="/images/jobs/fourth.png" alt="" />
+								<div className={styles.card_info}>
+									<h2 className={styles.card_title}>UX Writer</h2>
+									<FontAwesomeIcon className={styles.plus} icon={faPlus} />
+									<span>Mid-Level</span>
+								</div>{' '}
+							</div>
+							{/* Job Location */}
+							<div className={styles.location}>
+								<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
+								<p className={styles.description}>
+									<span>Remote</span> office in Lagos , Nigria
+								</p>
+							</div>
+							{/*Card Body */}
+							<p className={styles.card_body}>
+								Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
+								dummy text ever.
+							</p>
+						</div>
 					</div>
 
-					{/* Fifth Card*/}
-					<div className={styles.card}>
-						{/* Card Icon , Title , Position*/}
-						<div className={styles.header}>
-							<Image
-								className={styles.card_icon}
-								width={60}
-								height={50}
-								src="/images/jobs/fifth.png"
-								alt=""
-							/>
-							<div className={styles.card_info}>
-								<h2 className={styles.card_title}>Brand Designer</h2>
-                                <FontAwesomeIcon  className={styles.plus}icon={faPlus} />
-								<span>Senior</span>
-							</div>{' '}
-						</div>
-						{/* Job Location */}
-						<div className={styles.location}>
-							<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
-							<p className={styles.description}>
-								<span>Remote</span> office in California , USA
+					<div className={styles.row}>
+						{/* Fifth Card*/}
+						<div className={styles.card}>
+							{/* Card Icon , Title , Position*/}
+							<div className={styles.header}>
+								<Image className={styles.card_icon} width={60} height={50} src="/images/jobs/fifth.png" alt="" />
+								<div className={styles.card_info}>
+									<h2 className={styles.card_title}>Brand Designer</h2>
+									<FontAwesomeIcon className={styles.plus} icon={faPlus} />
+									<span>Senior</span>
+								</div>{' '}
+							</div>
+							{/* Job Location */}
+							<div className={styles.location}>
+								<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
+								<p className={styles.description}>
+									<span>Remote</span> office in California , USA
+								</p>
+							</div>
+							{/*Card Body */}
+							<p className={styles.card_body}>
+								Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
+								dummy text ever.
 							</p>
 						</div>
-						{/*Card Body */}
-						<p className={styles.card_body}>
-							Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
-							dummy text ever.
-						</p>
-					</div>
 
-					{/* Sixth Card  */}
-					<div className={styles.card}>
-						{/* Card Icon , Title , Position*/}
-						<div className={styles.header}>
-							<Image
-								className={styles.card_icon}
-								width={60}
-								height={50}
-								src="/images/jobs/sixth.png"
-								alt=""
-							/>
-							<div className={styles.card_info}>
-								<h2 className={styles.card_title}>UX Designer</h2>
-                                <FontAwesomeIcon  className={styles.plus}icon={faPlus} />
-								<span>Senior</span>
-							</div>{' '}
-						</div>
-						{/* Job Location */}
-						<div className={styles.location}>
-							<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
-							<p className={styles.description}>
-								<span>Remote</span> office in Ottawa , Ontario
+						{/* Sixth Card  */}
+						<div className={styles.card}>
+							{/* Card Icon , Title , Position*/}
+							<div className={styles.header}>
+								<Image className={styles.card_icon} width={60} height={50} src="/images/jobs/sixth.png" alt="" />
+								<div className={styles.card_info}>
+									<h2 className={styles.card_title}>UX Designer</h2>
+									<FontAwesomeIcon className={styles.plus} icon={faPlus} />
+									<span>Senior</span>
+								</div>{' '}
+							</div>
+							{/* Job Location */}
+							<div className={styles.location}>
+								<FontAwesomeIcon className={styles.card_fontawesome} icon={faLocationDot} />
+								<p className={styles.description}>
+									<span>Remote</span> office in Ottawa , Ontario
+								</p>
+							</div>
+							{/*Card Body */}
+							<p className={styles.card_body}>
+								Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
+								dummy text ever.
 							</p>
 						</div>
-						{/*Card Body */}
-						<p className={styles.card_body}>
-							Lorem Ipsum is simply printing and typesetting industry, Lorem Ipsum has been the industry's standard
-							dummy text ever.
-						</p>
 					</div>
 				</div>
 			</section>
